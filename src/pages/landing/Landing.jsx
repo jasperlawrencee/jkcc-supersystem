bcef8a3e01fe9224087c6cf3a9a0670e6d149ea4
jasperlawrencee <caballerojasper.55555@gmail.com>
2022-08-10 21:49:00 +0800

    created pages

---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./landing.scss"
+
+const Landing = () => {
+  return (
+    <div className='landing'><h1 className="title">Landing Page</h1></div>
+  )
+}
+
+export default Landing
